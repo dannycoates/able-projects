e8d36628c65a0d06079f1a7f617b5ff3e40d82da
Danny Coates <dannycoates@gmail.com>
2015-01-07 17:48:56 -0800

    3.2.1. contact

---
+++ README.md
@@ -0,0 +1,3 @@
+# Able project registry
+
+To add a project to the registry simply add its git url to `package.json`
---
+++ index.js
@@ -0,0 +1,25 @@
+var glob = require('glob')
+var path = require('path')
+var p = require('./package.json')
+
+function loadExperiments(projectName) {
+  var basePath = path.resolve(__dirname, 'node_modules', projectName)
+  var filenames = glob.sync(basePath + '/**/*.js')
+  var experiments = []
+  for (var i = 0; i < filenames.length; i++) {
+    var filename = filenames[i]
+    experiments.push(require(filename))
+  }
+  return experiments
+}
+
+var projects = {}
+var projectNames = Object.keys(p.dependencies)
+for (var i = 0; i < projectNames.length; i++) {
+  var name = projectNames[i]
+  if (name !== 'glob') {
+    projects[name] = loadExperiments(name)
+  }
+}
+
+module.exports = projects
